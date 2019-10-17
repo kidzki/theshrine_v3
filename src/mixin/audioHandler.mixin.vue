2212f4c6fa4275e56cebcaf20e249cf6325fad7c
kidzki <kidzki@outlook.de>
2019-10-18 01:06:54 +0200

    ADD: added audioHandler

---
+++ src/mixin/audioHandler.mixin.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "audioHandlerMixin",
+  methods: {
+    playSound(sound) {
+      if (sound) {
+        let audio = new Audio(sound);
+        audio.play();
+      }
+    }
+  }
+};
+</script>
