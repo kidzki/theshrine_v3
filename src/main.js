84362f5515b8313eb8fcad73725bfb14a9cd8766
Michael Kick <mi.kick@reply.de>
2019-10-25 13:54:11 +0200

    CHG: removed vue-meta

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
-import Meta from 'vue-meta';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
-Vue.use(Meta);
 Vue.config.productionTip = false;
 
 new Vue({
